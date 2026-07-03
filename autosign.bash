gentoo/gentoo-authority-key

commit 9954a7ac1b6f3f424fe73f123f81e5b315373db6

autosign.bash: use /usr/local/bin/openpgp-key-upload if available
/usr/local/bin/openpgp-key-upload will send to all known Gentoo keyservers
rather than the roundrobin.

The synchronisation proves brittle so let's be careful and use what
we tell devs to do.

Signed-off-by: Sam James <sam@gentoo.org>

autosign.bash
@@ -257,11 +257,24 @@ main() {
 		# send key updates to the keyserver
 		local retries=0
 		while [[ -s to-send.txt ]]; do
-			if gpg --batch --send-keys $(head -n 10 to-send.txt); then
-				tail -n +11 to-send.txt > to-send.txt.tmp &&
-				mv to-send.txt.tmp to-send.txt || die 'failure writing to-send.txt'
+			export OPENPGP_KEY_UPLOAD_KEYS=$(head -n 10 to-send.txt)
+
+			# Prefer the same script we use for developers to avoid
+			# bad luck with missing sychronisation.
+			if [[ -x /usr/local/bin/openpgp-key-upload ]]; then
+				if /usr/local/bin/openpgp-key-upload; then
+					tail -n +11 to-send.txt > to-send.txt.tmp &&
+					mv to-send.txt.tmp to-send.txt || die 'failure writing to-send.txt'
+				else
+					[[ $(( ++retries )) -ge 5 ]] && die 'send failure limit exceeded'
+				fi
 			else
-				[[ $(( ++retries )) -ge 5 ]] && die 'send failure limit exceeded'
+				if gpg --batch --send-keys ${OPENPGP_KEY_UPLOAD_KEYS}; then
+					tail -n +11 to-send.txt > to-send.txt.tmp &&
+					mv to-send.txt.tmp to-send.txt || die 'failure writing to-send.txt'
+				else
+					[[ $(( ++retries )) -ge 5 ]] && die 'send failure limit exceeded'
+				fi
 			fi
 		done
 	fi

commit 6711eef218ce393fd69d44b8eb6aa2ee87da0233

autosign.bash: fix openpgp-key-upload invocation
Fixes: 9954a7ac1b6f3f424fe73f123f81e5b315373db6
Signed-off-by: Sam James <sam@gentoo.org>

autosign.bash
@@ -262,7 +262,7 @@ main() {
 			# Prefer the same script we use for developers to avoid
 			# bad luck with missing sychronisation.
 			if [[ -x /usr/local/bin/openpgp-key-upload ]]; then
-				if /usr/local/bin/openpgp-key-upload; then
+				if gpg --export ${OPENPGP_KEY_UPLOAD_KEYS} | /usr/local/bin/openpgp-key-upload; then
 					tail -n +11 to-send.txt > to-send.txt.tmp &&
 					mv to-send.txt.tmp to-send.txt || die 'failure writing to-send.txt'
 				else