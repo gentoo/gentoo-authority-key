gentoo/gentoo-authority-key

commit dfa00dc31c2838e8bf89030d4b6df3c531bb5457

autosign.bash: rename intermediate/output files at startup
This makes it easier to debug later rather than possibly having some
stale files mixed from previous runs.

Signed-off-by: Sam James <sam@gentoo.org>

autosign.bash
@@ -219,6 +219,14 @@ main() {
 	# avoid running with old agent
 	gpgconf --kill all
 
+	# Keep the last run around for easier debugging, but always clean
+	# up existing ones first so it's clear which are fresh from a given
+	# run.
+	local filename
+	for filename in ldap.txt signed.txt to-send.txt ; do
+		mv "${filename}" "${filename}.old"
+	done
+
 	refresh_keys
 	get_ldap | sort -u > ldap.txt
 	pipestatus || die "failure writing ldap.txt, exited w/ $?"

commit 56435d997d42b039bd31ecd4db95547105573eb6

autosign.bash: silence rename attempts
For example, to-send.txt may not exist if there was no work to be done.

Fixes: dfa00dc31c2838e8bf89030d4b6df3c531bb5457
Signed-off-by: Sam James <sam@gentoo.org>

autosign.bash
@@ -224,7 +224,7 @@ main() {
 	# run.
 	local filename
 	for filename in ldap.txt signed.txt to-send.txt ; do
-		mv "${filename}" "${filename}.old"
+		mv "${filename}" "${filename}.old" &> /dev/null
 	done
 
 	refresh_keys